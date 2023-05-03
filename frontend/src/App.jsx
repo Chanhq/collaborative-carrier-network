e880c0835c035419d94e4b905d15496242ccacb0
Cong Chanh Quach <cong.quach@tuhh.de>
2023-05-03 19:32:11 +0200

    temporary frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,37 @@
-import { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Login } from './Login';
+import { Register_Carrier } from './Register_Carrier' ;
+import { Register_Auctioneer } from './Register_Auctioneer' ;
 
 function App() {
-  const url = '/api/test';
-  const [data, setData] = useState([]);
 
-  const fetchInfo = () => {
-    return fetch(url)
-      .then((res) => res.json())
-      .then((d) => setData(d));
-  };
+  const [currentForm, setCurrentForm] = useState('login');
 
-  useEffect(() => {
-    fetchInfo();
-  }, []);
+  const ToggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{data.greeting}</p>
-      </header>
-    </div>
-  );
+  if(loginCurrentForm === "login"){
+    return (
+      <div className="App">
+        <Login onFormSwitch={ToggleForm} />      
+      </div>
+    );
+  }else if(carrierCurrentForm === "register_carrier"){
+      return (
+        <div className="App">
+          <Register_Carrier onFormSwitch={carrierToggleForm} />
+        </div>
+      );
+  }else if(auctioneerCurrentForm === "register_auctioneer"){
+      return (
+        <div className="App">
+          <Register_Auctioneer onFormSwitch={auctioneerToggleForm} />
+        </div>
+      );
+  }  
 }
-
 export default App;
+
+
---
+++ frontend/src/Register_Carrier.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+export const Register_Carrier = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>Carrier Registration</h2>
+            <form className="carrier-register-form" onSubmit={handleSubmit}>
+                <label htmlFor="name">Enter your Full Name : </label>
+                <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Your Full Name" />
+                <label htmlFor="email">Enter your new Email :</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">Enter your new password :</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <label htmlFor="password">Re-enter your new password :</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="submit">Create Account and Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+    </div>
+    )
+}
