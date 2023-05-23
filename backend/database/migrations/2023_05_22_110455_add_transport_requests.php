25697e5fa030ab74a0a25bbddc1a695696145e5a
Cong Chanh Quach <cong.quach@tuhh.de>
2023-05-23 17:10:03 +0200

    Add transport request

---
+++ backend/database/migrations/2023_05_22_110455_add_transport_requests.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transport_requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+
+            $table->string('requester_name');
+            $table->string('pickup_location');
+            $table->string('destination');
+            $table->timestamp('request_date')->useCurrent();
+            $table->timestamps();
+            
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transport_requests', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+    }
+};
