bd2f58d73d22c61051973aaae3721f0960b102ee
sikhelifi <sirine.khelifi1@1und1.de>
2023-05-27 12:49:19 +0200

    add dummy users to the databank
    ST03-05

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -9,13 +9,9 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+public function run()
+{
+    \App\Models\User::factory(20)->create();
+}
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
 }
