eb08439aba9428ddd8bbd9b83bb962b12c52c2c1
Fin Bießler <fin.biessler@tuhh.de>
2023-05-28 20:34:49 +0200

    add transport request seeding

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Facades\Map;
 use App\Models\MapVertex;
+use App\Models\TransportRequest;
 use App\Models\User;
 use Fhaculty\Graph\Vertex;
 use Illuminate\Database\Seeder;
@@ -16,7 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        if(User::all()->count() === 0) {
+        if (User::all()->count() === 0) {
             User::factory(1)->create(['is_auctioneer' => true]);
             User::factory(19)->create(['is_auctioneer' => false]);
         }
@@ -27,6 +28,35 @@ class DatabaseSeeder extends Seeder
             $id = Str::remove(search: 'n', subject: $vertex->getId());
             if (MapVertex::find($id) === null) {
                 MapVertex::factory(1)->create(['id' => $id]);
+            }
+        }
+
+        if (TransportRequest::all()->count() === 0) {
+            /** @var User $user */
+            foreach (User::all() as $user) {
+                if ($user->isAuctioneer()){
+                    continue;
+                }
+
+                $transportRequests = [];
+                $destination_node_id = 0;
+                $origin_node_id = 0;
+
+                for ($i = 0; $i < 10; $i++) {
+                    do{
+                        $origin_node_id = random_int(0, 2576);
+                        do {
+                            $destination_node_id = random_int(0, 2576);
+                        } while ($origin_node_id === $destination_node_id);
+                    } while (in_array([$origin_node_id, $destination_node_id], $transportRequests));
+                    $transportRequest = new TransportRequest([
+                        'requester_name' => 'Some requester name',
+                        'origin_node' => $origin_node_id,
+                        'destination_node' => $destination_node_id,
+                    ]);
+                    $user->transportRequests()->save($transportRequest);
+                    $transportRequests[] = [$origin_node_id, $destination_node_id];
+                }
             }
         }
     }
