8c1a9716f9bf1788394ceeb93db6bf50b4a06d02
Fin Bießler <fin.biessler@tuhh.de>
2023-05-29 17:00:17 +0200

    creating base infrastucture f

---
+++ backend/app/Console/Commands/OptimalPathForCarrierTransportRequestSet.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Process;
+
+class OptimalPathForCarrierTransportRequestSet extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'carrier:optimal-path {carrier=2 : Id of the carrier }';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Calculate the optimal path for transport request set for a given carrier agent';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $carrierAgentId = $this->argument('carrier');
+        $user = User::find($carrierAgentId);
+
+        if ($user === null || $user->isAuctioneer()) {
+            $this->error('Could not find given user or is auctioneer! Aborting.');
+            return self::FAILURE;
+        }
+
+        $result = Process::run('python3 network/main.py');
+        $this->info($result->output());
+
+        return self::SUCCESS;
+    }
+}
