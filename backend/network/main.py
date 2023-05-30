28367939d688fa6b3ce2d540cdf3d0d840e636b8
Fin Bießler <fin.biessler@tuhh.de>
2023-05-30 17:51:00 +0200

    parse graphml and set all needed attributes

--- backend/network/main.py
+++ backend/network/main.py
@@ -3,7 +3,9 @@ import typer
 import json
 
 from typing import Optional
-from pygraphml import GraphMLParser
+
+
+from graphml_parser import GraphMLParser
 
 sys.path.append("../")
 
@@ -12,12 +14,11 @@ app = typer.Typer()
 @app.command()
 def optimalpath(transportrequests: Optional[str] = None):
     transportrequests = json.loads(transportrequests)
-    typer.echo(transportrequests)
 
     parser = GraphMLParser()
     g = parser.parse('../maps/default.graphml')
 
-    g.show()
+
 
 if __name__ == "__main__":
     app()
