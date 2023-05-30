98c3fcdaa256d2867432a84d0b0db3e81fb25845
Fin Bießler <fin.biessler@tuhh.de>
2023-05-30 17:51:27 +0200

    add method for generating new complete graph in viz command

--- backend/app/Console/Commands/VisualizeMap.php
+++ backend/app/Console/Commands/VisualizeMap.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Facades\Map;
+use Fhaculty\Graph\Graph;
+use Fhaculty\Graph\Vertex;
 use Graphp\GraphViz\GraphViz;
 use Illuminate\Console\Command;
 
@@ -17,5 +19,33 @@ class VisualizeMap extends Command
 
         $graphviz = new GraphViz();
         $graphviz->display($graph);
+    }
+
+    private function generateConnectedGraph(int $vertexCount)
+    {
+        $newGraph = new Graph();
+        $vertexCount++;
+
+        for ($i = 1; $i < $vertexCount; $i++) {
+            $newGraph->createVertex($i);
+        }
+
+        $edgeId = 1;
+        /** @var Vertex $currentVertex */
+        foreach ($newGraph->getVertices() as $currentVertex) {
+            $id = $currentVertex->getId();
+            /** @var Vertex $otherVertex */
+            foreach ($newGraph->getVertices() as $otherVertex) {
+                if ($id === $otherVertex->getId()) {
+                    continue;
+                } else {
+                    if (!$currentVertex->hasEdgeTo($otherVertex)) {
+                        $edge = $currentVertex->createEdge($otherVertex);
+                        $edge->setAttribute('weight', random_int(1,100));
+                        $edge->setAttribute('id', $edgeId++);
+                    }
+                }
+            }
+        }
     }
 }
