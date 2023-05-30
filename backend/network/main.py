038cf25e1bbc81990aec1a611b75fba0b58918a2
Fin Bießler <fin.biessler@tuhh.de>
2023-05-30 19:26:25 +0200

    started creating or_tools_data for describing the vhr problem

--- backend/network/main.py
+++ backend/network/main.py
@@ -1,24 +1,39 @@
 import sys
 import typer
-import json
 
+from mapper import transportrequest_mapper, graph_mapper
 from typing import Optional
-
-
+from pygraphml import Graph
 from graphml_parser import GraphMLParser
 
 sys.path.append("../")
 
 app = typer.Typer()
 
+
+def print_graph(g: Graph):
+    for node in g.nodes():
+        print('========' + node.id + '========')
+        for edge in node.edges():
+            print(edge.parent().id, edge.child().id, edge['weight'], edge.id)
+            print('----------------------------------------')
+
+
 @app.command()
 def optimalpath(transportrequests: Optional[str] = None):
-    transportrequests = json.loads(transportrequests)
-
     parser = GraphMLParser()
-    g = parser.parse('../maps/default.graphml')
+    graph = parser.parse('../maps/default.graphml')
 
+    or_tool_data = {
+        'distance_matrix': graph_mapper.to_distance_matrix(graph),
+        'pickups_deliveries': transportrequest_mapper.to_pickups_deliveries(transportrequests),
+        'depot': 1,
+        'num_vehicles': 1
+    }
 
+    print(or_tool_data)
+
+    print_graph(graph)
 
 if __name__ == "__main__":
     app()
---
+++ backend/network/mapper/graph_mapper.py
@@ -0,0 +1,5 @@
+from pygraphml import Graph
+
+
+def to_distance_matrix(graph: Graph) -> [[int]]:
+    print(graph)
