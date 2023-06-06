93aa685c335cea7b9beb24adc56378431923ffa9
Cong Chanh Quach <cong.quach@tuhh.de>
2023-06-06 18:18:57 +0200

    Fix MapVisualizer.jsx

--- frontend/src/components/Map/MapVisualizer.jsx
+++ frontend/src/components/Map/MapVisualizer.jsx
@@ -1,15 +1,20 @@
-import Sigma from 'sigma';
+import { Sigma } from 'react-sigma';
 import { fetchMapData } from '../../lib/api/map';
+import {AuthContext} from "../../lib/context/AuthContext";
+import { useContext } from 'react';
 
 function MapVisualizer() {
 
-    const graphData = fetchMapData();
+    const { user } = useContext(AuthContext);
+    const graph = fetchMapData(user.token);
+    console.log(graph);
+    
 
   return (
     <div>
       <Sigma
-        style={{ width: '100%', height: '500px' }} // Adjust the dimensions as needed
-        graph={graphData}
+        style={{ width: '100%', height: '500px' }} 
+        graph={graph}
         settings={{
           drawEdges: true,
           clone: false,
@@ -19,4 +24,41 @@ function MapVisualizer() {
   );
 }
 
-export default MapVisualizer;
+export default MapVisualizer; 
+
+/*import React from 'react';
+import { Sigma, RandomizeNodePositions, EdgeShapes } from 'react-sigma';
+
+function MapVisualizer() {
+  const graph = {
+    nodes: [
+      { id: 'A', label: 'Node A', x: 0, y: 0, size: 1, color: '#FF0000' },
+      { id: 'B', label: 'Node B', x: 100, y: 0, size: 1, color: '#00FF00' },
+      { id: 'C', label: 'Node C', x: 50, y: 100, size: 1, color: '#0000FF' }
+    ],
+    edges: [
+      { id: 'AB', source: 'A', target: 'B', color: '#FF0000' },
+      { id: 'BC', source: 'B', target: 'C', color: '#00FF00' },
+      { id: 'CA', source: 'C', target: 'A', color: '#0000FF' }
+    ]
+  };
+
+  return (
+    <Sigma
+      graph={graph}
+      style={{ width: '1000px', height: '600px' }}
+      settings={{
+        drawEdges: true,
+        drawEdgeLabels: true,
+        clone: false
+      }}
+    >
+      <RandomizeNodePositions />
+      <EdgeShapes default="curvedArrow" />
+    </Sigma>
+  );
+}
+
+export default MapVisualizer; */
+
+
--- frontend/src/lib/api/map.js
+++ frontend/src/lib/api/map.js
@@ -1,16 +1,15 @@
 // apiService.js
 
-import { useState } from 'react';
-import axios from 'axios';
+//import { useState } from 'react';
+import httpClient from "../infrastructure/http-client";
 
-export async function fetchMapData() {
-
-  const [graphData, setGraphData] = useState({ nodes: [], edges: [] });
+export async function fetchMapData(token) {
 
   try {
-    const response = await axios.get('/api/maps');
-    setGraphData(response.data);
-    return graphData;
+    let client = httpClient;
+    client.defaults.headers.get['Authorization'] = 'Bearer ' + token;
+    const response = await client.get('http://localhost/api/carrier-frontend/map');
+    return response;
   } catch (error) {
     console.error('Error fetching map data:', error);
     return null;
