060b22c8fa13fc7f55d1d353b12c89bd3dc2cee9
Fin Bießler <fin.biessler@tuhh.de>
2023-06-08 15:23:18 +0200

    fix fe

--- frontend/src/components/Map/MapVisualizer.jsx
+++ frontend/src/components/Map/MapVisualizer.jsx
@@ -1,103 +1,40 @@
-import { Sigma, RandomizeNodePositions, EdgeShapes } from 'react-sigma';
+import { Sigma } from 'react-sigma';
 import { fetchMapData } from '../../lib/api/map';
-import { AuthContext } from "../../lib/context/AuthContext";
-import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../lib/context/AuthContext';
+import React, { useContext, useEffect, useState } from 'react';
 
 function MapVisualizer() {
-  const { user } = useContext(AuthContext);
-  const [graph, setGraph] = useState({ edges: [], vertices: [] });
+	const { user } = useContext(AuthContext);
+	const [graph, setGraph] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const mapData = await fetchMapData(user.token);
-        
-        if (mapData) {
-          const { edges, vertices } = mapData;
-          console.log(mapData);
-          console.log(edges);
-          console.log(vertices);
-          
-          const edgesFormatted = edges.map((edge) => ({
-            weight: edge.weight,
-            source: edge.source,
-            target: edge.target,
-            isOnOptimalPath: edge.isOnOptimalPath,
-            size: 1,
-            color: '#000000'
-          }));
+	const fetchData = async () => {
+		try {
+			const mapData = await fetchMapData(user.token);
 
-          const verticesFormatted = vertices.map((vertex) => ({
-            id: vertex.id,
-            x: vertex.x,
-            y: vertex.y,
-            size: 5,
-            color: '#FF0000'
-          }));
+			if (mapData) {
+				setGraph(mapData);
+			}
+		} catch (error) {
+			console.error('Error fetching map data:', error);
+		}
+	};
 
-          setGraph({ edges: edgesFormatted, vertices: verticesFormatted });
-        }
-      } catch (error) {
-        console.error('Error fetching map data:', error);
-      }
-    };
+	useEffect(() => {
+		fetchData();
+	}, []);
 
-    fetchData();
-  }, [user.token]);
-
-  return (
-    <div>
-      <Sigma
-        graph={graph}
-        style={{ width: '1000px', height: '600px' }}
-        settings={{
-          drawEdges: true,
-          drawEdgeLabels: true,
-          clone: false
-        }}
-      >
-        <RandomizeNodePositions />
-        <EdgeShapes default="curvedArrow" />
-      </Sigma>
-      </div>
-  );
+	return (
+		graph &&
+		<Sigma
+			graph={graph}
+			settings={{
+				drawEdges: true,
+				drawEdgeLabels: true,
+				clone: false
+			}}
+		>
+		</Sigma>
+	);
 }
 
-export default MapVisualizer; 
- 
-
-/*import React from 'react';
-import { Sigma, RandomizeNodePositions, EdgeShapes } from 'react-sigma';
-
-function MapVisualizer() {
-  const graph = {
-    nodes: [
-      { id: 'A', label: 'Node A', x: 0, y: 0, size: 1, color: '#FF0000' },
-      { id: 'B', label: 'Node B', x: 100, y: 0, size: 1, color: '#00FF00' },
-      { id: 'C', label: 'Node C', x: 50, y: 100, size: 1, color: '#0000FF' }
-    ],
-    edges: [
-      { id: 'AB', source: 'A', target: 'B', color: '#FF0000' },
-      { id: 'BC', source: 'B', target: 'C', color: '#00FF00' },
-      { id: 'CA', source: 'C', target: 'A', color: '#0000FF' }
-    ]
-  };
-
-  return (
-    <Sigma
-      graph={graph}
-      style={{ width: '1000px', height: '600px' }}
-      settings={{
-        drawEdges: true,
-        drawEdgeLabels: true,
-        clone: false
-      }}
-    >
-      <RandomizeNodePositions />
-      <EdgeShapes default="curvedArrow" />
-    </Sigma>
-  );
-}
-
-export default MapVisualizer; */
-
+export default MapVisualizer;
--- frontend/src/lib/api/map.js
+++ frontend/src/lib/api/map.js
@@ -1,16 +1,16 @@
 // apiService.js
 
-import httpClient from "../infrastructure/http-client";
+import httpClient from '../infrastructure/http-client';
 
 export async function fetchMapData(token) {
 
-  try {
-    let client = httpClient;
-    client.defaults.headers.get['Authorization'] = 'Bearer ' + token;
-    const response = await client.get('http://localhost/api/carrier-frontend/map');
-    return response;
-  } catch (error) {
-    console.error('Error fetching map data:', error);
-    return null;
-  }
+	try {
+		let client = httpClient;
+		client.defaults.headers.get['Authorization'] = 'Bearer ' + token;
+		const response = await client.get('api/carrier-frontend/map');
+		return response.data.data.map;
+	} catch (error) {
+		console.error('Error fetching map data:', error);
+		return null;
+	}
 }
