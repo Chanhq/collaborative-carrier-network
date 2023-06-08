6cefaee2ab0eee248611b5f7763bd59ad557a81c
Cong Chanh Quach <cong.quach@tuhh.de>
2023-06-08 13:03:11 +0200

    Update MapVisualizer.jsx

--- frontend/src/components/Map/MapVisualizer.jsx
+++ frontend/src/components/Map/MapVisualizer.jsx
@@ -13,12 +13,15 @@ function MapVisualizer() {
         const mapData = await fetchMapData(user.token);
         
         if (mapData) {
-          const { edges, vertices } = mapData;
-          console.log(mapData);
-          console.log(edges);
-          console.log(vertices);
+          const { data: responseData } = mapData;
+          const { data: mapDataNested } = responseData;
+          const { map } = mapDataNested;
+          const { edges, vertices } = map;
+          const edgesArray = edges;
+          const verticesArray = vertices;
+
           
-          const edgesFormatted = edges.map((edge) => ({
+          const edgesFormatted = edgesArray.map((edge) => ({
             weight: edge.weight,
             source: edge.source,
             target: edge.target,
@@ -27,13 +30,16 @@ function MapVisualizer() {
             color: '#000000'
           }));
 
-          const verticesFormatted = vertices.map((vertex) => ({
+          
+          const verticesFormatted = verticesArray.map((vertex) => ({
             id: vertex.id,
             x: vertex.x,
             y: vertex.y,
             size: 5,
             color: '#FF0000'
           }));
+
+        
 
           setGraph({ edges: edgesFormatted, vertices: verticesFormatted });
         }
@@ -65,39 +71,4 @@ function MapVisualizer() {
 
 export default MapVisualizer; 
  
-
-/*import React from 'react';
-import { Sigma, RandomizeNodePositions, EdgeShapes } from 'react-sigma';
-
-function MapVisualizer() {
-  const graph = {
-    nodes: [
-      { id: 'A', label: 'Node A', x: 0, y: 0, size: 1, color: '#FF0000' },
-      { id: 'B', label: 'Node B', x: 100, y: 0, size: 1, color: '#00FF00' },
-      { id: 'C', label: 'Node C', x: 50, y: 100, size: 1, color: '#0000FF' }
-    ],
-    edges: [
-      { id: 'AB', source: 'A', target: 'B', color: '#FF0000' },
-      { id: 'BC', source: 'B', target: 'C', color: '#00FF00' },
-      { id: 'CA', source: 'C', target: 'A', color: '#0000FF' }
-    ]
-  };
-
-  return (
-    <Sigma
-      graph={graph}
-      style={{ width: '1000px', height: '600px' }}
-      settings={{
-        drawEdges: true,
-        drawEdgeLabels: true,
-        clone: false
-      }}
-    >
-      <RandomizeNodePositions />
-      <EdgeShapes default="curvedArrow" />
-    </Sigma>
-  );
-}
-
-export default MapVisualizer; */
 
