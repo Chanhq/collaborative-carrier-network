44bf39d85a7b7227ff0db76ba819f8a288f3446f
Fin Bießler <fin.biessler@tuhh.de>
2023-06-12 16:42:24 +0200

    add new auction_status column to transport_requests table

---
+++ backend/database/migrations/2023_06_12_143833_add_status_column_to_transport_request_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transport_requests', function (Blueprint $table) {
+            $table->enum('auction_status', ['pristine', 'selected_for_auction'])->default('pristine');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transport_requests', function (Blueprint $table) {
+            $table->dropColumn('auction_status');
+        });
+    }
+};
