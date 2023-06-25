5a12873d6f1e384b1adc9192886e311d2b51e385
Cong Chanh Quach <cong.quach@tuhh.de>
2023-06-25 15:07:51 +0200

    Add table for auction bidding

---
+++ backend/database/migrations/2023_06_25_121848_create_auction_bids_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('auction_bids', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('auction_id');
+            $table->unsignedBigInteger('carrier_id');
+            $table->decimal('bid_amount', 8, 2);
+            $table->timestamps();
+
+            $table->foreign('auction_id')->references('id')->on('auctions')->onDelete('cascade');
+            $table->foreign('carrier_id')->references('id')->on('carriers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('auction_bids');
+    }
+};
